9594d6623b3e4da65a54cc9cf688c0c4c89bc579
nemesis0804 <neelanjanaprakash26sri@gmail.com>
2026-07-04 01:24:13 +0530

    Time: 176 ms (82.56%) | Memory: 334 MB (66.67%) - LeetSync

---
+++ 3919-network-recovery-pathways/network-recovery-pathways.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+  long long INF = 4e18;
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+          int n = online.size();
+
+        vector<vector<pair<int,int>>> adj(n);
+        vector<int> indegree(n,0);
+
+        int mx = 0;
+
+        for(auto &e : edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            indegree[e[1]]++;
+            mx = max(mx,e[2]);
+        }
+
+        // Topological order
+        queue<int> q;
+        for(int i=0;i<n;i++)
+            if(indegree[i]==0)
+                q.push(i);
+
+        vector<int> topo;
+
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            topo.push_back(u);
+
+            for(auto &it:adj[u]){
+                if(--indegree[it.first]==0)
+                    q.push(it.first);
+            }
+        }
+
+        auto check = [&](int limit)->bool{
+
+            vector<long long> dist(n,INF);
+            dist[0]=0;
+
+            for(int u:topo){
+
+                if(dist[u]==INF) continue;
+
+                if(u!=0 && u!=n-1 && !online[u]) continue;
+
+                for(auto &[v,w]:adj[u]){
+
+                    if(w<limit) continue;
+
+                    if(v!=n-1 && v!=0 && !online[v]) continue;
+
+                    if(dist[u]+w<dist[v]){
+                        dist[v]=dist[u]+w;
+                    }
+                }
+            }
+
+            return dist[n-1]<=k;
+        };
+
+        int lo=0,hi=mx;
+        int ans=-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+
+            if(check(mid)){
+                ans=mid;
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+
+        return ans;
+        
+    }
+};
